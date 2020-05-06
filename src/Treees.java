0968774bff8e88110acf6b135ad06c584fbfe37c
unknown <you@example.com>
2020-05-05 19:51:59 -0500

    tercer ejerciio

---
+++ src/Treees.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Treees {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+           int a; 
+		
+		Scanner scan=new Scanner (System.in);
+		 System.out.println("Escribir primer número");
+		 a=scan.nextInt();
+		
+		 if(a>0) {
+				//verdadera
+				System.out.println("Positivo");
+			         }
+		
+			else {
+				System.out.println("Negativo");
+		
+			}
+	}
+
+}
